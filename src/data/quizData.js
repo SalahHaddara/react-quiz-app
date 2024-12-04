b828c7c2a19c8fbf6aaed73cec3875785826e85d
Salah Haddara <Salahhaddara02@gmail.com>
2024-12-04 21:24:08 +0200

    create quiz metadata

---
+++ src/data/quizData.js
@@ -0,0 +1,24 @@
+export const quizData = [
+    {
+        id: 1,
+        title: 'Maths',
+        description: 'Calculations questions',
+        questions: [
+            {
+                id: 1,
+                type: "text-input",
+                question: "What is 2 + 2?",
+                correctAnswer: "4",
+                points: 5
+            },
+            {
+                id: 2,
+                type: "multiple-choice",
+                question: "Which is greater?",
+                options: ["5", "3", "1", "2"],
+                correctAnswer: "5",
+                points: 5
+            }
+        ]
+    }
+];
