f5a92298f9c4a8954ac24e0f34aa570c769637bd
Salah Haddara <Salahhaddara02@gmail.com>
2024-12-04 22:49:30 +0200

    create initialStates

--- src/context/QuizContext.jsx
+++ src/context/QuizContext.jsx
@@ -2,3 +2,10 @@ import React, {createContext, useContext, useReducer} from 'react';
 
 const QuizContext = createContext();
 
+const initialState = {
+    currentQuizIndex: null,
+    currentQuestionIndex: 0,
+    userAnswer: "",
+    score: 0,
+    quizComplete: false
+};
