0da0fae2812656fcafd7230f6408a36991034435
Salah Haddara <Salahhaddara02@gmail.com>
2024-12-05 08:05:32 +0200

    add a startQuiz function to dispatch an action

--- src/hooks/useQuiz.js
+++ src/hooks/useQuiz.js
@@ -1,4 +1,8 @@
+import {useQuizContext} from '../context/QuizContext';
+
 export function useQuiz() {
+    const {state, dispatch} = useQuizContext();
+
     const checkAnswer = (userAnswer, correctAnswer, type) => {
         if (type === 'text-input') {
             return userAnswer.toLowerCase().trim() === correctAnswer.toLowerCase().trim();
@@ -6,5 +10,7 @@ export function useQuiz() {
         return userAnswer === correctAnswer;
     };
 
-
+    const startQuiz = (index) => {
+        dispatch({type: 'START_QUIZ', payload: index});
+    };
 }
